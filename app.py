87a4fd23c107a1a27004c805ff5625f27c085ada
Varun072006 <varunshanmugam643@gmail.com>
2025-08-25 12:22:45 +0530

    update

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ import streamlit as st
 import numpy as np
 import pickle  
 
-model = pickle.load("taxi_fare_model.sav")
+with open("taxi_fare_model.sav", "rb") as f:
+    model = pickle.load(f)
 
 st.title("🚕 Taxi Fare Prediction")
 
 distance = st.number_input("Enter Distance (km):", min_value=0.0, value=15.0, step=0.5)
-predicted_fare = model.predict([[distance]])[0]
 
-st.success(f"Predicted Fare for {distance} km: ₹ {predicted_fare:.2f}")
+if st.button("Predict Fare"):
+    predicted_fare = model.predict([[distance]])[0]
+    st.success(f"Predicted Fare for {distance} km: ₹ {predicted_fare:.2f}")
